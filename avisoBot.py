ae85a385e1ea8f3e8da35e46f0fc6db38e8247d9
mastero <castro.alejandro17@gmail.com>
2022-10-27 20:00:51 -0500

    sdasd

--- avisoBot.py
+++ avisoBot.py
@@ -6,6 +6,15 @@ from bs4 import BeautifulSoup
 import time
 import webbrowser
 
+def telegram_bot_sendtext(bot_message):
+
+    bot_token = '5627768852:AAH5afLkApzk_l3r8NQWqDtaPUkHAMeE-bA'
+    bot_chatID = '600556'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+
+    return response.json()
 
 url = requests.get("https://www.cyberpuerta.mx/Computo-Hardware/Componentes/Procesadores/Procesadores-para-PC/Procesador-AMD-Ryzen-5-5600-S-AM4-3-50GHz-Six-Core-32MB-L3-Cache-con-Disipador-Wraith-Stealth.html")
 
@@ -17,17 +26,6 @@ print(precio)
 precioInicial = float(precio)
 
 precioDeseado = 2700
-
-
-def telegram_bot_sendtext(bot_message):
-
-    bot_token = '5627768852:AAH5afLkApzk_l3r8NQWqDtaPUkHAMeE-bA'
-    bot_chatID = '600556'
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-
-    response = requests.get(send_text)
-
-    return response.json()
 
 if precioInicial < precioDeseado:
     test =  telegram_bot_sendtext(f"Oferta, bajo el precio a: {'$ '+str(precioInicial)}\nEnlace: https://www.cyberpuerta.mx/Computo-Hardware/Componentes/Procesadores/Procesadores-para-PC/Procesador-AMD-Ryzen-5-5600-S-AM4-3-50GHz-Six-Core-32MB-L3-Cache-con-Disipador-Wraith-Stealth.html")
